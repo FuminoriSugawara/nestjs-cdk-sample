b1b0a749085594729d9794a93b1be15d4f939ea5
Fuminori Sugawara <fuminori.sugawara@gmail.com>
2022-12-28 13:59:10 +0900

    Modify docker-compose.yml

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,15 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
 import { AllExceptionsFilter } from './allException.filter';
-import { existsSync } from 'fs';
-import { config } from 'dotenv';
 
-const envFile = '../.env';
-if (existsSync(envFile)) {
-  config({
-    path: envFile,
-  });
-}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.useLogger(app.get(Logger));
